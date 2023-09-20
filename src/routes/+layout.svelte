4245600182558de8de79c65514bd92e821e24814
Andres Galvan <andresgalfajar@gmail.com>
2023-09-20 10:21:54 -0500

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.postcss';
 	import Header from './Header.svelte';
 	import './styles.css';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="app">
